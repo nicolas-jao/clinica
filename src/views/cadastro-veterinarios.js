85170187fe4e7586736254b9b2a39c83c4159882
Iago <iagoalmeidaoliveira7@gmail.com>
2026-06-23 02:32:55 -0300

    Refatoração CadastroVeterinarios

--- src/views/cadastro-veterinarios.js
+++ src/views/cadastro-veterinarios.js
@@ -2,23 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import Stack from '@mui/material/Stack';
-
 import Card from '../components/card';
 import FormGroup from '../components/form-group';
 
 import { mensagemSucesso, mensagemErro } from '../components/toastr';
-
 import '../custom.css';
 
-import axios from 'axios';
-import { BASE_URL } from '../config/axios';
+import httpClient from '../config/axios';
 
 function CadastroVeterinarios() {
   const { idParam } = useParams();
-
   const navigate = useNavigate();
 
-  const baseURL = `${BASE_URL}/veterinarios`;
+  const baseURL = `/veterinarios`;
 
   const [id, setId] = useState('');
   const [nome, setNome] = useState('');
@@ -35,10 +31,8 @@ function CadastroVeterinarios() {
   const [uf, setUf] = useState('');
   const [senha, setSenha] = useState('');
 
-  const [dados, setDados] = React.useState([]);
-
-  function inicializar() {
-    if (idParam == null) {
+  function inicializar(dadosCarregados = null) {
+    if (dadosCarregados == null) {
       setId('');
       setNome('');
       setCpf('');
@@ -53,82 +47,63 @@ function CadastroVeterinarios() {
       setCidade('');
       setUf('');
       setSenha('');
-
     } else {
-      setId(dados.id);
-      setNome(dados.nome);
-      setCpf(dados.cpf);
-      setCrmv(dados.crmv);
-      setTelefone(dados.telefone);
-      setEmail(dados.email);
-      setCep(dados.cep);
-      setLogradouro(dados.logradouro);
-      setNumero(dados.numero);
-      setComplemento(dados.complemento);
-      setBairro(dados.bairro);
-      setCidade(dados.cidade);
-      setUf(dados.uf);
-      setSenha(dados.senha);
+      setId(dadosCarregados.id);
+      setNome(dadosCarregados.nome);
+      setCpf(dadosCarregados.cpf);
+      setCrmv(dadosCarregados.crmv);
+      setTelefone(dadosCarregados.telefone);
+      setEmail(dadosCarregados.email);
+      setCep(dadosCarregados.cep);
+      setLogradouro(dadosCarregados.logradouro);
+      setNumero(dadosCarregados.numero);
+      setComplemento(dadosCarregados.complemento);
+      setBairro(dadosCarregados.bairro);
+      setCidade(dadosCarregados.cidade);
+      setUf(dadosCarregados.uf);
+      setSenha(dadosCarregados.senha);
     }
   }
 
   async function salvar() {
-    let data = { id, nome, cpf, crmv, telefone, email, cep, logradouro, numero, complemento, bairro, cidade, uf, senha};
-    data = JSON.stringify(data);
+    const data = { id, nome, cpf, crmv, telefone, email, cep, logradouro, numero, complemento, bairro, cidade, uf, senha };
+    
     if (idParam == null) {
-      await axios
-        .post(baseURL, data, {
-          headers: { 'Content-Type': 'application/json' },
-        })
+      await httpClient.post(baseURL, data)
         .then(function (response) {
-          mensagemSucesso(`Veterinario ${nome} cadastrado com sucesso!`);
+          mensagemSucesso(`Veterinário ${nome} cadastrado com sucesso!`);
           navigate(-1);
         })
         .catch(function (error) {
-          mensagemErro(error.response.data);
+          mensagemErro("Erro ao cadastrar: " + (error.response?.data || "Verifique os dados"));
         });
     } else {
-      await axios
-        .put(`${baseURL}/${idParam}`, data, {
-          headers: { 'Content-Type': 'application/json' },
-        })
+      await httpClient.put(`${baseURL}/${idParam}`, data)
         .then(function (response) {
           mensagemSucesso(`Veterinário ${nome} alterado com sucesso!`);
           navigate(-1);
         })
         .catch(function (error) {
-          mensagemErro(error.response.data);
+          mensagemErro("Erro ao alterar: " + (error.response?.data || "Verifique os dados"));
         });
     }
   }
 
   async function buscar() {
-    if(idParam != null){
-      await axios.get(`${baseURL}/${idParam}`).then((response) => {
-        setDados(response.data);
+    if (idParam != null) {
+      await httpClient.get(`${baseURL}/${idParam}`).then((response) => {
+        inicializar(response.data);
       });
-      setId(dados.id);
-      setNome(dados.nome);
-      setCpf(dados.cpf);
-      setCrmv(dados.crmv);
-      setTelefone(dados.telefone);
-      setEmail(dados.email);
-      setCep(dados.cep);
-      setLogradouro(dados.logradouro);
-      setNumero(dados.numero);
-      setComplemento(dados.complemento);
-      setBairro(dados.bairro);
-      setCidade(dados.cidade);
-      setUf(dados.uf);
-      setSenha(dados.senha);
     }
   }
 
   useEffect(() => {
-    buscar(); // eslint-disable-next-line
-  }, [id]);
+    if (idParam != null) {
+      buscar();
+    }
+   
+  }, [idParam]);
 
-  if (!dados) return null;
   return (
     <div className='container'>
       <Card title='Cadastro de Veterinário'>
@@ -136,153 +111,48 @@ function CadastroVeterinarios() {
           <div className='col-lg-12'>
             <div className='bs-component'>
               <FormGroup label='Nome: *' htmlFor='inputNome'>
-                <input
-                  type='text'
-                  id='inputNome'
-                  value={nome}
-                  className='form-control'
-                  name='nome'
-                  onChange={(e) => setNome(e.target.value)}
-                />
+                <input type='text' id='inputNome' value={nome} className='form-control' name='nome' onChange={(e) => setNome(e.target.value)} />
               </FormGroup>
               <FormGroup label='CPF: *' htmlFor='inputCpf'>
-                <input
-                  type='text'
-                  minLength='11'
-                  maxLength='11'
-                  id='inputCpf'
-                  value={cpf}
-                  className='form-control'
-                  name='cpf'
-                  onChange={(e) => setCpf(e.target.value)}
-                />
+                <input type='text' minLength='11' maxLength='11' id='inputCpf' value={cpf} className='form-control' name='cpf' onChange={(e) => setCpf(e.target.value)} />
               </FormGroup>
               <FormGroup label='CRMV: *' htmlFor='inputCrmv'>
-                <input
-                  type='text'
-                  id='inputCrmv'
-                  value={crmv}
-                  className='form-control'
-                  name='crmv'
-                  onChange={(e) => setCrmv(e.target.value)}
-                />
+                <input type='text' id='inputCrmv' value={crmv} className='form-control' name='crmv' onChange={(e) => setCrmv(e.target.value)} />
               </FormGroup>
               <FormGroup label='Telefone: *' htmlFor='inputTelefone'>
-                <input
-                  type='number'
-                  id='inputTelefone'
-                  value={telefone}
-                  className='form-control'
-                  name='telefone'
-                  onChange={(e) => setTelefone(e.target.value)}
-                />
+                <input type='number' id='inputTelefone' value={telefone} className='form-control' name='telefone' onChange={(e) => setTelefone(e.target.value)} />
               </FormGroup>
               <FormGroup label='Email: *' htmlFor='inputEmail'>
-                <input
-                  type='email'
-                  id='inputEmail'
-                  value={email}
-                  className='form-control'
-                  name='email'
-                  onChange={(e) => setEmail(e.target.value)}
-                />
+                <input type='email' id='inputEmail' value={email} className='form-control' name='email' onChange={(e) => setEmail(e.target.value)} />
               </FormGroup>
               <FormGroup label='CEP:' htmlFor='inputCep'>
-                <input
-                  type='number'
-                  id='inputCep'
-                  value={cep}
-                  className='form-control'
-                  name='cep'
-                  onChange={(e) => setCep(e.target.value)}
-                />
+                <input type='number' id='inputCep' value={cep} className='form-control' name='cep' onChange={(e) => setCep(e.target.value)} />
               </FormGroup>
               <FormGroup label='Logradouro:' htmlFor='inputLogradouro'>
-                <input
-                  type='text'
-                  id='inputLogradouro'
-                  value={logradouro}
-                  className='form-control'
-                  name='logradouro'
-                  onChange={(e) => setLogradouro(e.target.value)}
-                />
+                <input type='text' id='inputLogradouro' value={logradouro} className='form-control' name='logradouro' onChange={(e) => setLogradouro(e.target.value)} />
               </FormGroup>
               <FormGroup label='Número:' htmlFor='inputNumero'>
-                <input
-                  type='number'
-                  id='inputNumero'
-                  value={numero}
-                  className='form-control'
-                  name='numero'
-                  onChange={(e) => setNumero(e.target.value)}
-                />
+                <input type='number' id='inputNumero' value={numero} className='form-control' name='numero' onChange={(e) => setNumero(e.target.value)} />
               </FormGroup>
               <FormGroup label='Complemento:' htmlFor='inputComplemento'>
-                <input
-                  type='text'
-                  id='inputComplemento'
-                  value={complemento}
-                  className='form-control'
-                  name='complemento'
-                  onChange={(e) => setComplemento(e.target.value)}
-                />
+                <input type='text' id='inputComplemento' value={complemento} className='form-control' name='complemento' onChange={(e) => setComplemento(e.target.value)} />
               </FormGroup>
               <FormGroup label='Bairro:' htmlFor='inputBairro'>
-                <input
-                  type='text'
-                  id='inputBairro'
-                  value={bairro}
-                  className='form-control'
-                  name='bairro'
-                  onChange={(e) => setBairro(e.target.value)}
-                />
+                <input type='text' id='inputBairro' value={bairro} className='form-control' name='bairro' onChange={(e) => setBairro(e.target.value)} />
               </FormGroup>
               <FormGroup label='Cidade:' htmlFor='inputCidade'>
-                <input
-                  type='text'
-                  id='inputCidade'
-                  value={cidade}
-                  className='form-control'
-                  name='cidade'
-                  onChange={(e) => setCidade(e.target.value)}
-                />
+                <input type='text' id='inputCidade' value={cidade} className='form-control' name='cidade' onChange={(e) => setCidade(e.target.value)} />
               </FormGroup>
               <FormGroup label='UF:' htmlFor='inputUf'>
-                <input
-                  type='text'
-                  id='inputUf'
-                  value={uf}
-                  className='form-control'
-                  name='uf'
-                  onChange={(e) => setUf(e.target.value)}
-                />
+                <input type='text' id='inputUf' value={uf} className='form-control' name='uf' onChange={(e) => setUf(e.target.value)} />
               </FormGroup>
               <FormGroup label='Senha: *' htmlFor='inputSenha'>
-                <input
-                  type='password'
-                  id='inputSenha'
-                  value={senha}
-                  className='form-control'
-                  name='senha'
-                  onChange={(e) => setSenha(e.target.value)}
-                />
+                <input type='password' id='inputSenha' value={senha} className='form-control' name='senha' onChange={(e) => setSenha(e.target.value)} />
               </FormGroup>
 
               <Stack spacing={1} padding={1} direction='row'>
-                <button
-                  onClick={salvar}
-                  type='button'
-                  className='btn btn-success'
-                >
-                  Salvar
-                </button>
-                <button
-                  onClick={inicializar}
-                  type='button'
-                  className='btn btn-danger'
-                >
-                  Cancelar
-                </button>
+                <button onClick={salvar} type='button' className='btn btn-success'>Salvar</button>
+                <button onClick={() => inicializar()} type='button' className='btn btn-danger'>Cancelar</button>
               </Stack>
             </div>
           </div>
